1965d878e6953534fa0da1e79a8dfef27953141a
chaidk <chaidk.1992@gmail.com>
2016-06-03 18:37:05 +0800

    add db

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-echo date("m/d/y h:m:s");
+
+// time
+	// echo date("m/d/y h:m:s");
+// post
+
+
+// db
+	$query = "select name,message,private from message";
+	$result = db_connection($query);
+	$message=array();
+	while ($row = mysql_fetch_array($result,MYSQL_ASSOC)){
+	// print_r ($row);
+	array_push($message,array('name'=>$row['name'],'message'=>$row['message'],'private'=>$row['private']));
+	}
+	echo json_encode(array("message"=>$message));
+	function db_connection($query){
+	mysql_connect('127.0.0.1','root','qwqw1212')OR die('Could not connect to database.');
+	mysql_select_db('message_board');
+	return mysql_query($query);
+	}
+
 ?>
--- scripts/js.js
+++ scripts/js.js
@@ -5,6 +5,32 @@ var $close=$("img#close");
 $close.click(function(){
 	$note.fadeOut(300);
 });
+//message
+var $message=$("div.message");
+var $text=$("div.text");
+getMessage();
+
+
+// get
+function getMessage(){
+	$.getJSON("index.php",function(json){
+		if(json.message.length > 0){
+			// $("div.text").empty();
+			$.each(json.message,function(){
+				if(this['private']=='0'){
+					$message.prepend("<div class='text'>"+'<h2>'+this['name']+'</h2>'+'<p>'+this['message']+'</p>'+'</div>');
+					
+				}
+					// alert("!");
+				// $("div.text").fadeIn(300);				
+			});
+			// $("div.text").each(function(){
+			// 	// 
+
+			// })
+		}
+	})
+}
 //send
 var $pub=$(".buttonPub");
 var $pri=$(".buttonPri");
@@ -14,15 +40,6 @@ $pub.click(function(){
 $pri.click(function(){
 	$message.fadeIn(300);
 });
-
-//message
-var $message=$("div.message");
-var $text=$("<div class='text'></div>");
-var $h2="<h2></h2>";
-var $p="<p></p>";
-
-
-
 
 
 
