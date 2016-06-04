c39b211805c8adcfbc5d942544b0813e40bfbbef
chaidk <chaidk.1992@gmail.com>
2016-06-04 21:51:09 +0800

    basic functions complete

--- index.php
+++ index.php
@@ -3,7 +3,38 @@
 // time
 	// echo date("m/d/y h:m:s");
 // post
+if($_GET){	
+	if ($_GET['action'] == 'submit') {
+	
+		$name = htmlspecialchars($_GET['name']);
+		$message = htmlspecialchars($_GET['message']);
+		$private = htmlspecialchars($_GET['private']);
 
+		// if(preg_match('/[^\w\s]/i', $fname) || preg_match('/[^\w\s]/i', $lname)) {
+		// 	fail('Invalid name provided.');
+		// }
+		// if( empty($fname) || empty($lname) ) {
+		// 	fail('Please enter a first and last name.');
+		// }
+		// if( empty($gender) ) {
+		// 	fail('Please select a gender.');
+		// }
+		// if( empty($minutes) || empty($seconds) ) {
+		// 	fail('Please enter minutes and seconds.');
+		// }
+		
+		$query = "INSERT INTO message SET name='$name', message='$message', private='$private'";
+		$result = db_connection($query);
+		
+		if ($result) {
+			$msg = "name: ".$name." added successfully" ;
+			success($msg);
+		} else {
+			fail('Insert failed.');
+		}
+		exit;
+	}
+}
 
 // db
 	$query = "select name,message,private from message";
@@ -18,6 +49,12 @@
 	mysql_connect('127.0.0.1','root','qwqw1212')OR die('Could not connect to database.');
 	mysql_select_db('message_board');
 	return mysql_query($query);
+	}
+		function fail($message) {
+		die(json_encode(array('status' => 'fail', 'message' => $message)));
+	}
+	function success($message) {
+		die(json_encode(array('status' => 'success', 'message' => $message)));
 	}
 
 ?>
--- scripts/js.js
+++ scripts/js.js
@@ -1,46 +1,101 @@
 $(function(){
-//note
+// note
 var $note=$("div.note");
 var $close=$("img#close");
 $close.click(function(){
 	$note.fadeOut(300);
 });
-//message
+// message
+getMessage();
 var $message=$("div.message");
 var $text=$("div.text");
-getMessage();
 
 
 // get
 function getMessage(){
 	$.getJSON("index.php",function(json){
 		if(json.message.length > 0){
-			// $("div.text").empty();
+			$("div.message").empty();
 			$.each(json.message,function(){
 				if(this['private']=='0'){
 					$message.prepend("<div class='text'>"+'<h2>'+this['name']+'</h2>'+'<p>'+this['message']+'</p>'+'</div>');
-					
 				}
-					// alert("!");
-				// $("div.text").fadeIn(300);				
+			
 			});
-			// $("div.text").each(function(){
-			// 	// 
-
-			// })
 		}
 	})
 }
-//send
+
+// submit
 var $pub=$(".buttonPub");
 var $pri=$(".buttonPri");
+var $sub=$("#submit");
+var $p0=$("#private0");
+var $p1=$("#private1");
 $pub.click(function(){
-	$message.fadeIn(300);
+	$p0.attr("class","1");
+// 	$message.fadeIn(500);
+// 	var $data = $(".form :input").serializeArray();
+// 	$.get("index.php",$data,function(json){
+// 			if (json.status == "fail") {
+// 				// alert(json.message);
+// 			}
+// 			if (json.status == "success") {
+// 				// alert(json.message);
+// 				clearInputs();
+// 			$("div.text").css({top:'+=100px'});	
+// 				setTimeout(function(){getMessage()},200);
+// 			}		
+// 	}, "json");
+// 	return false;
 });
 $pri.click(function(){
-	$message.fadeIn(300);
+	$p0.attr("class","0");
+// 	$message.fadeIn(500);
+// 	var $data = $(".form :input").serializeArray();
+// 	$.get("index.php",$data,function(json){
+// 			if (json.status == "fail") {
+// 				// alert(json.message);
+// 			}
+// 			if (json.status == "success") {
+// 				// alert(json.message);
+// 				clearInputs();
+// 			if ($data.private=='0'){
+// 			$("div.text").css({top:'+=100px'});	
+// 			}
+// 			setTimeout(function(){getMessage()},200);
+// 			}		
+// 	}, "json");
+// 	return false;
 });
 
+var $select=$("input[type=radio][checked]").val();
+$sub.click(function(){
+	alert($p0.attr("class"));
+	$message.fadeIn(500);
+	var $data = $(".form :input").serializeArray();
+	$.get("index.php",$data,function(json){
+			if (json.status == "fail") {
+				// alert(json.message);
+			}
+			if (json.status == "success") {
+				// alert(json.message);
+				clearInputs();
+			if ($p0.attr("class")=="1"){
+			$("div.text").css({top:'+=100px'});	
+			};
+			setTimeout(function(){getMessage()},200);
+			}		
+	}, "json");
+	return false;
+});
+
+function clearInputs(){
+	$("#name").val('');
+	$("#message").val('');
+}
+
+// validate
 
 
 
